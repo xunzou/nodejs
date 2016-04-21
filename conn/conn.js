0ea4dda7d4fd345c2d9972a7df326b903d8714a7
xunzou <xunzou@gmail.com>
2016-04-21 18:04:54 +0800

    'conn'

--- conn/conn.js
+++ conn/conn.js
@@ -10,18 +10,49 @@ conn.connect();
 
 var insertSQL = 'insert into user(name,password) values("xunzou1",123456),("haha1",123456)';
 var selectSQL = 'select * from user limit 10';
-var deleteSQL = 'delete from t_user';
-var updateSQL = 'update t_user set name="conan update"  where name="conan"';
+var deleteSQL = 'delete from user where name = "xunzou"';
+//delete from friends where user_name = 'simaopig';
+var updateSQL = 'update user set name="你大爷" where name="xunzou1"';
 
-conn.query(insertSQL,function (err, results) {
+/*conn.query(insertSQL,function (err, result) {
 	if (err) {
 		console.log(err)
 		return
 	};
 	console.log('--------------------------INSERT----------------------------');
-	console.log('INSERT ID:',results)
+	console.log('INSERT ID:',result)
+	console.log('------------------------------------------------------------');
+	// body...
+})*/
+
+/*conn.query(selectSQL,function(err,result){
+	if (err) {
+		console.log(err)
+		return
+	};
+	console.log('--------------------------SELECT----------------------------');
+	console.log(result)
+	console.log('------------------------------------------------------------');
+	// body...
+})*/
+/*conn.query(deleteSQL,function(err,result){
+	if (err) {
+		console.log(err)
+		return
+	};
+	console.log('--------------------------DELETE----------------------------');
+	console.log(result)
+	console.log('------------------------------------------------------------');
+	// body...
+})*/
+conn.query(updateSQL,function(err,result){
+	if (err) {
+		console.log(err)
+		return
+	};
+	console.log('--------------------------UPDDATE----------------------------');
+	console.log(result)
 	console.log('------------------------------------------------------------');
 	// body...
 })
-
 //connection.end();
