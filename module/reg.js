9d59ee05fd147f24acfef625548990b56103dda2
xunzou <xunzou@gmail.com>
2016-04-30 22:56:28 +0800

    bootstrap

---
+++ module/reg.js
@@ -0,0 +1,87 @@
+var conn = require('../conn')
+var mysql = require('mysql')
+var pool = mysql.createPool(conn);
+console.log(conn)
+pool.getConnection(function(err, connection) {
+	// Use the connection
+	connection.query('SELECT * FROM user', function(err, rows) {
+		if (err) {
+			console.log(err)
+			return
+		};
+
+		console.log('--------------------------SELECT----------------------------');
+		console.log(rows)
+		console.log('------------------------------------------------------------');
+
+		// And done with the connection.
+		connection.release();
+
+		// Don't use the connection here, it has been returned to the pool.
+	});
+});
+
+
+function Reg(user) {
+	this.name = user.name;
+	this.password = user.password;
+	this.email = user.email;
+};
+
+module.exports = Reg
+
+Reg.prototype = {
+	saveUser: function(callback) {
+		var self = this;
+		//要存入数据库的用户信息文档
+		//var user =  this.name,this.password
+		pool.getConnection(function(err, connection) {
+
+			var insertSQL = 'insert into user(name,password) values("' + self.name + '",' + self.password + ')'
+			console.log(insertSQL)
+			// Use the connection
+			connection.query(insertSQL, function(err, rows) {
+				if (err) {
+					console.log(err,44)
+					connection.release();
+					return callback(err)
+				};
+
+				console.log('--------------------------INSERT----------------------------');
+				//console.log(rows)
+				callback(null,rows)
+				console.log('------------------------------------------------------------');
+
+				// And done with the connection.
+				connection.release();
+
+				// Don't use the connection here, it has been returned to the pool.
+			});
+		});
+
+	},
+	getUser: function(name, callback) {
+		pool.getConnection(function(err, connection) {
+			var selectSQL = 'select * from user where name="'+ name +'"'
+			console.log(selectSQL)
+			// Use the connection
+			connection.query(selectSQL, function(err, rows) {
+				if (err) {
+					console.log(err,68)
+					connection.release();
+					return callback(err)
+				};
+
+				console.log('--------------------------SELECT----------------------------');
+				//console.log(rows)
+				callback(null,rows)
+				console.log('------------------------------------------------------------');
+
+				// And done with the connection.
+				connection.release();
+
+				// Don't use the connection here, it has been returned to the pool.
+			});
+		});
+	}
+}
--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,25 @@
-//router 
+//router
+var Reg = require('../module/reg.js');
 
 module.exports = function(app) {
+
+	function checkLogin(req, res, next) {
+		if (!req.session.user) {
+			req.flash('error', '未登录!');
+			return res.redirect('/login');
+		}
+		next();
+	}
+
+	function checkNotLogin(req, res, next) {
+		if (req.session.user) {
+			req.flash('error', '已登录!');
+			return res.redirect('back'); //返回之前的页面
+		}
+		next();
+	}
+
+
 	/* GET home page. */
 	app.get('/', function(req, res) {
 		res.render('index', {
@@ -8,11 +27,53 @@ module.exports = function(app) {
 		});
 	});
 
+
+	app.get('/reg', checkNotLogin)
 	app.get('/reg', function(req, res) {
 		res.render('reg', {
 			title: 'reg'
 		});
 	});
+	app.post('/reg', checkNotLogin)
+	app.post('/reg', function(req, res) {
+		var name = req.body.name,
+			password = req.body.password,
+			email = req.body.email;
+		var reg = new Reg({
+			name: name,
+			password: password,
+			email: email
+		})
+		console.log(name, password, email)
+		reg.getUser(name, function(err, user) {
+			console.log(err, user)
+			if (user.length) {
+				console.log('用户已存在!')
+				return res.redirect('/reg'); //返回注册页
+			};
+			reg.saveUser(function(err, user) {
+				if (err) {
+					console.log(err)
+					return
+				};
+				console.log(user)
+				res.redirect('/reg'); //返回注册页
+			})
+
+		})
+	})
+
+	app.get('/login', checkNotLogin)
+	app.get('/login', function(req, res) {
+		res.render('login', {
+			title: 'login'
+		});
+	});
+	app.post('/login', checkNotLogin)
+	app.post('/login', function(req, res) {
+		
+	});
+
 	// define the home page route
 	app.get('/help', function(req, res) {
 		res.send('Birds home page');
