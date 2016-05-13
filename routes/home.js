cbcc705eb59a8dbe67f65b31745d377bdf44aba7
xunzou <xunzou@gmail.com>
2016-05-14 00:29:39 +0800

    fiexd routes.js

--- routes/home.js
+++ routes/home.js
@@ -2,28 +2,29 @@
 var ArticleList = require('../module/articleList.js');
 
 exports.getHomePost = function(req, res) {
-	var userId = req.session.userId,
-		post = new Post({
-			userId: userId
-		});
-	post.getCate(function(err, data) {
-		if (err) {
-			console.log(err)
-			req.flash('error', '出错了');
-			return res.redirect('/post'); //返回文章页
-		};
-		if (data) {
-			console.log(data)
-			res.render('post', {
-				title: 'post',
-				cate: data,
-				success: req.flash('success').toString(),
-				error: req.flash('error').toString(),
-				user: req.session.user
-			});
-		};
+	var userId = req.session && req.session.userId;
+	var article = new ArticleList({
+		userId: userId
 	})
+	article.getArticle(function(err, rows) {
+		var allData = {
+			error: null,
+			result: null
+		}
+		if (err) {
+			//console.log(err)
+			req.flash('error', '出错了');
+			allData.error = err
+			//return res.end(JSON.stringify(err));
+		};
+		if (rows) {
+			allData.result = rows
+			//return res.end(JSON.stringify(rows));
+		};
+		//return res.json(allData);
+		return res.end(JSON.stringify(allData));
 
+	})
 };
 
 exports.getLatestPost = function(req, res) {
@@ -33,7 +34,7 @@ exports.getLatestPost = function(req, res) {
 			error: null,
 			result: null
 		}
-		console.log(rows)
+		//console.log(rows)
 		if (err) {
 			console.log(err)
 			req.flash('error', '出错了');
